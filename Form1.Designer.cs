619e24d90526cf1cb0992a6fcbea333acaeb84a0
Murathan <murathanonuryilmaz@gmail.com>
2025-12-11 04:50:27 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Sandbox {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.deviceList = new System.Windows.Forms.ComboBox();
+            this.deviceLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.modeLabel = new System.Windows.Forms.Label();
+            this.modeList = new System.Windows.Forms.ComboBox();
+            this.volumeBar = new System.Windows.Forms.TrackBar();
+            this.fishKeyLabel = new System.Windows.Forms.Label();
+            this.intKeyLabel = new System.Windows.Forms.Label();
+            this.fishKey = new System.Windows.Forms.Label();
+            this.intKey = new System.Windows.Forms.Label();
+            this.fishKeyButton = new System.Windows.Forms.Button();
+            this.intKeyButton = new System.Windows.Forms.Button();
+            this.logSound = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // deviceList
+            // 
+            this.deviceList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.deviceList.Location = new System.Drawing.Point(149, 161);
+            this.deviceList.Name = "deviceList";
+            this.deviceList.Size = new System.Drawing.Size(305, 24);
+            this.deviceList.TabIndex = 1;
+            this.deviceList.TabStop = false;
+            this.deviceList.SelectedIndexChanged += new System.EventHandler(this.deviceList_SelectedIndexChanged);
+            // 
+            // deviceLabel
+            // 
+            this.deviceLabel.AutoSize = true;
+            this.deviceLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.deviceLabel.Location = new System.Drawing.Point(16, 163);
+            this.deviceLabel.Name = "deviceLabel";
+            this.deviceLabel.Size = new System.Drawing.Size(113, 20);
+            this.deviceLabel.TabIndex = 0;
+            this.deviceLabel.Text = "Audio Device:";
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.startButton.Location = new System.Drawing.Point(342, 211);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(113, 59);
+            this.startButton.TabIndex = 2;
+            this.startButton.TabStop = false;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // modeLabel
+            // 
+            this.modeLabel.AutoSize = true;
+            this.modeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.modeLabel.Location = new System.Drawing.Point(16, 28);
+            this.modeLabel.Name = "modeLabel";
+            this.modeLabel.Size = new System.Drawing.Size(114, 20);
+            this.modeLabel.TabIndex = 0;
+            this.modeLabel.Text = "Fishing Mode:";
+            // 
+            // modeList
+            // 
+            this.modeList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeList.Location = new System.Drawing.Point(149, 26);
+            this.modeList.Name = "modeList";
+            this.modeList.Size = new System.Drawing.Size(120, 24);
+            this.modeList.TabIndex = 3;
+            this.modeList.TabStop = false;
+            // 
+            // volumeBar
+            // 
+            this.volumeBar.LargeChange = 1;
+            this.volumeBar.Location = new System.Drawing.Point(13, 214);
+            this.volumeBar.Maximum = 5;
+            this.volumeBar.Name = "volumeBar";
+            this.volumeBar.Size = new System.Drawing.Size(290, 56);
+            this.volumeBar.TabIndex = 5;
+            this.volumeBar.TabStop = false;
+            // 
+            // fishKeyLabel
+            // 
+            this.fishKeyLabel.AutoSize = true;
+            this.fishKeyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.fishKeyLabel.Location = new System.Drawing.Point(16, 73);
+            this.fishKeyLabel.Name = "fishKeyLabel";
+            this.fishKeyLabel.Size = new System.Drawing.Size(101, 20);
+            this.fishKeyLabel.TabIndex = 0;
+            this.fishKeyLabel.Text = "Fishing Key:";
+            // 
+            // intKeyLabel
+            // 
+            this.intKeyLabel.AutoSize = true;
+            this.intKeyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.intKeyLabel.Location = new System.Drawing.Point(16, 118);
+            this.intKeyLabel.Name = "intKeyLabel";
+            this.intKeyLabel.Size = new System.Drawing.Size(103, 20);
+            this.intKeyLabel.TabIndex = 0;
+            this.intKeyLabel.Text = "Interact Key:";
+            // 
+            // fishKey
+            // 
+            this.fishKey.AutoSize = true;
+            this.fishKey.Location = new System.Drawing.Point(146, 76);
+            this.fishKey.Name = "fishKey";
+            this.fishKey.Size = new System.Drawing.Size(75, 16);
+            this.fishKey.TabIndex = 7;
+            this.fishKey.Text = "F4 (Default)";
+            // 
+            // intKey
+            // 
+            this.intKey.AutoSize = true;
+            this.intKey.Location = new System.Drawing.Point(146, 121);
+            this.intKey.Name = "intKey";
+            this.intKey.Size = new System.Drawing.Size(75, 16);
+            this.intKey.TabIndex = 7;
+            this.intKey.Text = "F5 (Default)";
+            // 
+            // fishKeyButton
+            // 
+            this.fishKeyButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.fishKeyButton.Location = new System.Drawing.Point(261, 69);
+            this.fishKeyButton.Name = "fishKeyButton";
+            this.fishKeyButton.Size = new System.Drawing.Size(153, 35);
+            this.fishKeyButton.TabIndex = 8;
+            this.fishKeyButton.Text = "Bind Fishing";
+            this.fishKeyButton.UseVisualStyleBackColor = false;
+            this.fishKeyButton.Click += new System.EventHandler(this.fishKeyButton_Click);
+            // 
+            // intKeyButton
+            // 
+            this.intKeyButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.intKeyButton.Location = new System.Drawing.Point(261, 112);
+            this.intKeyButton.Name = "intKeyButton";
+            this.intKeyButton.Size = new System.Drawing.Size(153, 35);
+            this.intKeyButton.TabIndex = 8;
+            this.intKeyButton.Text = "Bind Interact";
+            this.intKeyButton.UseVisualStyleBackColor = false;
+            this.intKeyButton.Click += new System.EventHandler(this.intKeyButton_Click);
+            // 
+            // logSound
+            // 
+            this.logSound.AutoSize = true;
+            this.logSound.Location = new System.Drawing.Point(20, 260);
+            this.logSound.Name = "logSound";
+            this.logSound.Size = new System.Drawing.Size(130, 20);
+            this.logSound.TabIndex = 9;
+            this.logSound.TabStop = false;
+            this.logSound.Text = "Log Sound Level";
+            this.logSound.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(478, 302);
+            this.Controls.Add(this.logSound);
+            this.Controls.Add(this.intKeyButton);
+            this.Controls.Add(this.fishKeyButton);
+            this.Controls.Add(this.intKey);
+            this.Controls.Add(this.fishKey);
+            this.Controls.Add(this.volumeBar);
+            this.Controls.Add(this.modeList);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.intKeyLabel);
+            this.Controls.Add(this.fishKeyLabel);
+            this.Controls.Add(this.modeLabel);
+            this.Controls.Add(this.deviceLabel);
+            this.Controls.Add(this.deviceList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sandbox";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.volumeBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox deviceList;
+        private System.Windows.Forms.Label deviceLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label modeLabel;
+        private System.Windows.Forms.ComboBox modeList;
+        private System.Windows.Forms.TrackBar volumeBar;
+        private System.Windows.Forms.Label fishKeyLabel;
+        private System.Windows.Forms.Label intKeyLabel;
+        private System.Windows.Forms.Label fishKey;
+        private System.Windows.Forms.Label intKey;
+        private System.Windows.Forms.Button fishKeyButton;
+        private System.Windows.Forms.Button intKeyButton;
+        private System.Windows.Forms.CheckBox logSound;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Linq;
+using NAudio.CoreAudioApi;
+using System.Collections.Generic;
+
+namespace Sandbox {
+    public partial class Form1 : Form {
+        // KEYBOARD HOOKS
+        [DllImport("user32.dll")] public static extern IntPtr SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll")] public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        [DllImport("user32.dll")] public static extern int CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")] public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        [DllImport("user32.dll")] public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")] public static extern bool SetForegroundWindow(IntPtr focusWindow);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)] public static extern uint MapVirtualKey(uint uCode, uint uMapType);
+
+        private const int WH_KEYBOARD_LL = 13; //Low-level keyboard hook
+        private const int WM_KEYDOWN = 0x0100; //0X0101 KEYUP
+        private IntPtr hookHandle = IntPtr.Zero;
+        private HookProc hookProcDelegate; //SO IT LIVES WITH FORM
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam); //PUBLIC
+        //MY VARIABLES
+        private static bool canFish = false;
+        private static bool isStarted = false;
+        private static int bindingKey = -1;
+        private static int fishButton = 115;
+        private static int intButton = 116;
+        private static int currentDevice = 0;
+        private static int counter = 0;
+
+        //FORM SETTINGS
+        public Form1() {
+            InitializeComponent();
+            hookProcDelegate = new HookProc(KeyboardHookProc);
+            modeList.Items.Add("Automatic");
+            modeList.Items.Add("Manual");
+            modeList.SelectedIndex = 0;
+            using (var enumerator = new MMDeviceEnumerator()) {
+                var devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToList();
+                foreach (var device in devices) {
+                    deviceList.Items.Add(device);
+                }
+                deviceList.SelectedIndex = 0;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+        //LOADS FORM AND STARTS HOOK
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e); //Does the Form1_Load
+            hookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, hookProcDelegate, IntPtr.Zero, 0);
+        }
+        //UNHOOKS WHEN FORM CLOSES
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            // Unhook the keyboard hook when the form is
+            if (hookHandle != IntPtr.Zero) {
+                UnhookWindowsHookEx(hookHandle);
+            }
+            base.OnFormClosing(e);
+        }
+        //VKCODE SPECIAL CASES
+        public string GetSpecialKeyName(int vkCode) {
+            switch (vkCode) {
+                case 0x70: return "F1";
+                case 0x71: return "F2";
+                case 0x72: return "F3";
+                case 0x73: return "F4";
+                case 0x74: return "F5";
+                case 0x75: return "F6";
+                case 0x76: return "F7";
+                case 0x77: return "F8";
+                case 0x78: return "F9";
+                case 0x79: return "F10";
+                case 0x7A: return "F11";
+                case 0x7B: return "F12";
+                case 0x20: return "Space";
+                case 0x09: return "Tab";
+                case 0x10: return "Shift";
+                case 0x11: return "Ctrl";
+                case 0x12: return "Alt";
+                case 0x1B: return "Esc";
+                default: return null;
+            }
+        }
+        //KEYBINDER
+        private int KeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam) {
+            if (bindingKey == -1) {
+                //SKIPS
+            }
+            else {
+                Console.WriteLine("Working");
+                if (nCode >= 0 && wParam == (IntPtr) WM_KEYDOWN) {
+                    int vkCode = Marshal.ReadInt32(lParam);
+                    if (vkCode == 0x0D) { //Enter
+                        return CallNextHookEx(hookHandle, nCode, wParam, lParam);
+                    }
+                    uint result = MapVirtualKey((uint)vkCode, 2);
+                    if (bindingKey == 0) {
+                        fishButton = vkCode;
+                        if (GetSpecialKeyName(vkCode) != null) {
+                            fishKey.Text = GetSpecialKeyName(vkCode);
+                        }
+                        else if (result != 0) {
+                            fishKey.Text = ((char)result).ToString();
+                        }
+                    }
+                    else if (bindingKey == 1) {
+                        intButton = vkCode;
+                        if (GetSpecialKeyName(vkCode) != null) {
+                            intKey.Text = GetSpecialKeyName(vkCode);
+                        }
+                        else if (result != 0) {
+                            intKey.Text = ((char)result).ToString();
+                        }
+                    }
+                    bindingKey = -1;
+                    EnablerDisabler();
+                    fishKeyButton.Text = "Bind Fishing";
+                    intKeyButton.Text = "Bind Interact";
+                }
+            }
+            return CallNextHookEx(hookHandle, nCode, wParam, lParam);
+        }
+        //HOOK THE FISH - canFish
+
+        private static async void HookFish() {
+            Random random = new Random();
+            //PRESS F5
+            int rnd = random.Next(0203, 0886);
+            await Task.Delay(rnd);
+            keybd_event((byte)intButton, 0, 0x0000, UIntPtr.Zero);
+            //PULL F5
+            rnd = random.Next(0022, 0057);
+            await Task.Delay(rnd);
+            keybd_event((byte)intButton, 0, 0x0002, UIntPtr.Zero);
+            Console.WriteLine("Hook Fish");
+            //PRESS F4
+            rnd = random.Next(1133, 2256);
+            await Task.Delay(rnd);
+            keybd_event((byte)fishButton, 0, 0x0000, UIntPtr.Zero);
+            //PULL F4
+            rnd = random.Next(0024, 0054);
+            await Task.Delay(rnd);
+            keybd_event((byte)fishButton, 0, 0x0002, UIntPtr.Zero);
+            Console.WriteLine("Cast Hook");
+            await Task.Delay(1000);
+            canFish = true;
+        }
+        //LISTENER - isStarted // canFish
+        private void Timer1_Tick(object sender, EventArgs e) {
+            if (!isStarted) return;
+            List<MMDevice> devices;
+            using (var enumerator = new MMDeviceEnumerator()) {
+                devices = enumerator.EnumerateAudioEndPoints(DataFlow.Render, DeviceState.Active).ToList();
+            }
+            counter++;
+            if (counter > 150) {
+                counter = 0;
+                IntPtr focusWindow = FindWindow(null, "World of Warcraft");
+                if (focusWindow != IntPtr.Zero) {
+                    SetForegroundWindow(focusWindow);
+                    HookFish();
+                }
+            }
+            if (logSound.Checked) {
+                Console.WriteLine(devices[currentDevice].AudioMeterInformation.MasterPeakValue);
+            }
+            if (devices[currentDevice].AudioMeterInformation.MasterPeakValue > 0.10 && canFish) {
+                if (logSound.Checked) Console.WriteLine("Passed threshold");
+                canFish = false;
+                counter = 0;
+                IntPtr focusWindow = FindWindow(null, "World of Warcraft");
+                if (focusWindow != IntPtr.Zero) {
+                    SetForegroundWindow(focusWindow);
+                    HookFish();
+                }
+            }
+        }
+        //START BUTTON
+        private void startButton_Click(object sender, EventArgs e) {
+            EnablerDisabler();
+            startButton.Enabled = !startButton.Enabled;
+            isStarted = !isStarted;
+            if (isStarted) {
+                startButton.Text = "Stop";
+                IntPtr focusWindow = FindWindow(null, "World of Warcraft");
+                if (focusWindow != IntPtr.Zero) {
+                    SetForegroundWindow(focusWindow);
+                    HookFish();
+                }
+            }
+            else {
+                canFish = false;
+                startButton.Text = "Start";
+            }
+        }
+        //ENABLING-DISABLING OBJECTS
+        private void EnablerDisabler() {
+            modeList.Enabled = !modeList.Enabled;
+            fishKeyButton.Enabled = !fishKeyButton.Enabled;
+            intKeyButton.Enabled = !intKeyButton.Enabled;
+            deviceList.Enabled = !deviceList.Enabled;
+            volumeBar.Enabled = !volumeBar.Enabled;
+            startButton.Enabled = !startButton.Enabled;
+        }
+        //KEY SELECTER
+        private void fishKeyButton_Click(object sender, EventArgs e) {
+            fishKeyButton.Text = "Press a key...";
+            bindingKey = 0;
+            EnablerDisabler();
+        }
+        private void intKeyButton_Click(object sender, EventArgs e) {
+            intKeyButton.Text = "Press a key...";
+            bindingKey = 1;
+            EnablerDisabler();
+        }
+        //DEVICE SELECTER
+        private void deviceList_SelectedIndexChanged(object sender, EventArgs e) {
+            currentDevice = deviceList.SelectedIndex;
+        }
+    }
+}
+
+/*
+Process[] processes = Process.GetProcesses();
+foreach (Process p in processes) {
+    if (!String.IsNullOrEmpty(p.MainWindowTitle)) {
+        //p.CloseMainWindow() or Kill();
+        comboBox1.Items.Add(p.MainWindowTitle);
+    }
+}
+if (vkCode == (int)Keys.F5)
+*/
